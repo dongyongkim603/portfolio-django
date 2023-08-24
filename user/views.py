8b1efd435844ac00078959950a277b405c0c4753
John <john@beeyonder.com>
2023-08-24 13:37:17 -0400

    added urls, serializers, and functions

--- user/views.py
+++ user/views.py
@@ -6,8 +6,8 @@ from rest_framework.response import Response
 from .models import User
 from .serializers import ForumSerializer
 
-class LatestForumsList(APIView):
+class AllUsersList(APIView):
     def get(self, request, format=None):
         users = User.objects.all()[0:4]
-        serializer = ForumSerializer(User, many=True)
+        serializer = ForumSerializer(users, many=True)
         return Response(serializer.data)
