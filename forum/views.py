dbd856df591334628576433eed272cb5cb8397c0
John <dongyongkim8@gmail.com>
2023-08-29 17:06:59 -0400

    added generic 400 bad request to comment post

--- forum/views.py
+++ forum/views.py
@@ -39,7 +39,10 @@ class Comments(APIView):
         return Response(serializer.data, status=status.HTTP_200_OK)
     
     def post(self, request, format=None):
+        print(request.data)
         serializer = CommentSerializer(data=request.data)
+        print(serializer)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
