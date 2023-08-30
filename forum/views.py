c397ed620cb78ac55a75b64cb3dc5401e1c9bbd4
John <dongyongkim8@gmail.com>
2023-08-30 10:10:13 -0400

    added authentication

--- forum/views.py
+++ forum/views.py
@@ -3,24 +3,32 @@ from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.authentication import TokenAuthentication
 from rest_framework import status
 
 from .models import Forum, Comment, Category
 from .serializers import ForumSerializer, CommentSerializer, CategorySerializer
 
 class AllCategroiesList(APIView):
+    authentication_classes = [] 
+
     def get(self, request, format=None):
         categories = Category.objects.all()[0:100]
         serializer = CategorySerializer(categories, many=True)
         return Response(serializer.data)
 
 class LatestForumsList(APIView):
+    authentication_classes = [] 
+
     def get(self, request, format=None):
         forums = Forum.objects.all()[0:4]
         serializer = ForumSerializer(forums, many=True)
         return Response(serializer.data)
 
 class ForumDetail(APIView):
+    authentication_classes = [] 
+
     def get_object(self, category_slug, forum_slug):
         try:
             return Forum.objects.filter(category__slug=category_slug).get(slug=forum_slug)
@@ -33,6 +41,9 @@ class ForumDetail(APIView):
         return Response(serializer.data)
 
 class Comments(APIView):
+  
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
     def get(self, request, format=None):
         comments = Comment.objects.all()[0:40]
         serializer = CommentSerializer(comments, many=True)
